1d4224154c1448f92e36de1566b7b8f9bc0c808c
Julia Kim <juliaykim6@gmail.com>
2021-01-18 14:49:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,14 +12,10 @@ end
 
 def long_planeteer_calls(array)
   array.any? do |word|
-    if word.size > 4
-      return true
-    else
-      return false
+    word.size > 4
 end
 end
 
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
