285e47dbbacfc8ae0e06d3305d6d70b9f7faa4c0
Julia Kim <juliaykim6@gmail.com>
2021-01-18 14:30:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.map.with_index(1) do |index, name|
+  array.map.with_index(1) do |name, index|
     puts "#{index}. #{name}"
   end
 end
