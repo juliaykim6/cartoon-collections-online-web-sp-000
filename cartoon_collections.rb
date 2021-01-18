5976434e52e3d51416504ce5a5b5ac6bd7bbfa35
Julia Kim <juliaykim6@gmail.com>
2021-01-18 14:45:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,11 +11,11 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.map do |word|
+  array.select do |word|
     if word.size > 4
       return true
     else
-      return false 
+      return false
 end
 end
 
