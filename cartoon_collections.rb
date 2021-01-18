af09d140821246e1c688e39cd6ca26c6dc8a50a9
Julia Kim <juliaykim6@gmail.com>
2021-01-18 14:51:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,11 @@ end
 end
 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.detect do |word|
+    word == cheese_types
+    
 end
