c2aabcbb905519c89d2e418c926ec6307b20212d
Julia Kim <juliaykim6@gmail.com>
2021-01-18 14:37:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,13 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  names.map do |word|
+    if word.length > 4
+      return true
+    else
+      false
+    end
 end
 
 def find_the_cheese# code an argument here
